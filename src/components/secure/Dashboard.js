a15259365e80438b629def0f28ca4c1c254fd1bb
haki-projects <30647767+haki-projects@users.noreply.github.com>
2017-08-11 11:43:06 +0200

    Added a user profile box
    Added a user profile box so users have access to their info

--- src/components/secure/Dashboard.js
+++ src/components/secure/Dashboard.js
@@ -5,13 +5,50 @@ import { Link } from 'react-router';
 class Dashboard extends React.Component {
 	render() {
 		return (
-			<div>
-				<h1>Dashboard</h1>
+			<div className='container'>
+				<h1 className='text-center'>Your Dashboard</h1>
+				<div className='row text-center'>
+					<div className='col-sm-4'>
+						<div className='card'>
+							<div className='card-header'>Your Name </div>
+							<div className="row align-items-center">
+								<div className="col">
+									<img className='profile-pic ' src={require('../../../public/images/dummy_profile.png')} alt='profile'></img>
+								</div>
+							</div>
+							<div className='card-block'>
+							<ul className='list-group list-group-flush'>
+								<li className='list-group-item'>
+									<p className='card-text'>Bio:Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+									Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis
+									 dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellen
+										tesque eu, pretium quis, sem. Nulla consequat massa quis enim. </p>
+									</li>
+								<li className='list-group-item'>Member Since: </li>
+								<li className='list-group-item'>Last Logged in: </li>
+							</ul>
+							<div className='row'>
+								<div className='col'># of Entries</div>
+								<div className='col'># of Edits</div>
+								<div className='col'># of Logins</div>
+							</div>
+							<div className='row'>
+								<Link to='/#' className='col'>see more</Link>
+							</div>
+
+						</div>
+
+					</div>
+					</div>
+					<div className='col-sm-8 justify-content-center'>
+						Dashboard charts
+					</div>
 				Welcome
 				<br/>
 				<Link to='/profile'>Profile</Link>
 				<br/>
 				<Link to='/logout'>Logout</Link>
+			</div>
 			</div>
 		)
 	}
