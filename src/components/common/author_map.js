661b8cf275c91a88e70ba7085f0f06471f9e902c
haki-projects <30647767+haki-projects@users.noreply.github.com>
2017-10-06 17:08:39 +0200

    Fixed publisher Markers and added Paths for markers
    Fixed the publisher Markers and added functionality for the Button to trigger the marker paths.

--- src/components/common/author_map.js
+++ src/components/common/author_map.js
@@ -54,35 +54,33 @@ class AuthorMap extends Component {
     const margin = {top:10, left:100, right:100, bottom:10}
     const width = 1028 - margin.left - margin.right;
     const height = 500 - margin.top - margin.bottom;
-    const node = this.node
+    const node = this.node;
+    const markers = this.props.markerData;
 
+    const zoom = d3.zoom()
+    .scaleExtent([0.2, 10])
+    .on('zoom', this.zoomFunction);
 
+    const svgContainer = select(node)
+    .style('background-color', '#575e70')
+    .style('border', '2px solid steelblue')
+    .call(zoom)
+    .append('g');
 
     const worldData = feature(this.props.worldMapData, this.props.worldMapData.objects.countries).features;
 
     const projection = geoMercator()
       .translate([width / 2, height / 2]);
 
-    const path = geoPath().projection(projection);
+    const gPath = geoPath().projection(projection);
 
-    //define zoom behavior
-    const zoom = d3.zoom()
-        .scaleExtent([0.2, 10])
-        .on('zoom', this.zoomFunction);
-
-    const svgContainer = select(node)
-    .style('background-color', '#575e70')
-    .style('border', '10px solid steelblue')
-    .call(zoom);
-
-
-    const worldMap = select(node)
+    const worldMap = svgContainer
       .selectAll('countries')
       .data(worldData)
-      .enter().append('path')
+      .enter().append('svg:g').append('path')
       .attr('class', 'country')
       .attr('id', d => {return '_' + d.id;})
-      .attr('d', path)
+      .attr('d', gPath)
       .attr('fill', '#1E9244')
       .attr('stroke', '#f6f6f6')
       .attr('stroke-width', '.3')
@@ -96,72 +94,148 @@ class AuthorMap extends Component {
  })
 
 
-//  const authorNames = select('.author-details').append('svg')
-//           .attr('width', 400)
-//           .attr('height', 200)
-//           .style('background-color', 'grey')
-//           .append('g')
-//           .attr('transform', 'translate(0,0)')
-//           .data(this.props.markerData)
-//           .enter().append('text')
-
- const birthplaceMarkers = select(node)
+ const birthplaceMarkers = svgContainer
       .selectAll('birthplace')
       .data(this.props.markerData)
-      .enter().append('circle');
+      .enter().append('circle')
+      .attr('r', 3)
+      .attr('fill', 'red')
+      .attr('id', d => {return '_' + d.id;})
+      .attr('cx', d => { return projection([d.birthplace_longitude, d.birthplace_latitude])[0]})
+      .attr('cy', d => {return projection([d.birthplace_longitude, d.birthplace_latitude])[1]});
 
-const publisherMarkers = select(node)
-      .selectAll('publisher')
-      .data(this.props.markerData)
-      .enter().append('circle');
-
-const lastWorkplaceMarkers = select(node)
+const lastWorkplaceMarkers = svgContainer
       .selectAll('workplace')
       .data(this.props.markerData)
-      .enter().append('circle');
+      .enter().append('circle')
+      .attr('r', 3)
+      .attr('fill', 'brown')
+      .attr('id', d => {return '_' + d.id;})
+      .attr('cx', d => { return projection([d.last_workplace_longitude, d.last_workplace_latitude])[0]})
+      .attr('cy', d => {return projection([d.last_workplace_longitude, d.last_workplace_latitude])[1]});
 
-const birthplaces = birthplaceMarkers.attr('r', 3)
-            .attr('fill', 'red')
-            .attr('id', d => {return '_' + d.id;})
-            .attr('cx', d => { return projection([d.birthplace_longitude, d.birthplace_latitude])[0]})
-            .attr('cy', d => {return projection([d.birthplace_longitude, d.birthplace_latitude])[1]})
 
-            const publisher = publisherMarkers.attr('r', 3)
-            .attr('fill', 'black')
-            .attr('id', d => {return '_' + d.id;})
-            .attr('cx', d => { return projection([d.birthplace_longitude, d.birthplace_latitude])[0]})
-            .attr('cy', d => {return projection([d.birthplace_longitude, d.birthplace_latitude])[1]})
+const publishers = this.getPublishersData(this.props.markerData);
+const publisherMarkers = svgContainer
+      .selectAll('publisher')
+      .data(publishers)
+      .enter().append('circle')
+      .attr('r', 3)
+      .attr('fill', 'black')
+      .attr('id', d => {return '_' + d.id;})
+      .attr('cx', d => { return projection([d.publisher_longitude, d.publisher_latitude])[0]})
+      .attr('cy', d => {return projection([d.publisher_longitude, d.publisher_latitude])[1]});
+ }
 
-            const lastWorkplace = lastWorkplaceMarkers.attr('r', 3)
-            .attr('fill', 'brown')
-            .attr('id', d => {return '_' + d.id;})
-            .attr('cx', d => { return projection([d.last_workplace_longitude, d.last_workplace_latitude])[0]})
-            .attr('cy', d => {return projection([d.last_workplace_longitude, d.last_workplace_latitude])[1]})
-
-  }
 
   onAuthorButtonClick(author) {
-    console.log(author);
-  }
-
-  createLines(author) {
-    const width = 1028 - 200;
-    const height = 500 - 20;
+    const margin = {top:10, left:100, right:100, bottom:10}
+    const width = 1028 - margin.left - margin.right;
+    const height = 500 - margin.top - margin.bottom;
     const projection = geoMercator()
     .translate([width / 2, height / 2]);
-    //check if there is a marker for publisher 2, if so, create a line. if not, move to the next if statement
+
+  const gPath = geoPath().projection(projection);
+    const node = this.node;
+    const svgContainer = select(node);
+    const authorGeoJson = this.getGeoJsonObj(author);
+    console.log(authorGeoJson);
+
+    var authorPaths = svgContainer
+      .append('path')
+      .attr('id', d => {return '_' + author.id;})
+      .attr('d', gPath(authorGeoJson))
+      .style('stroke', 'white')
+      .attr('fill', 'none');
+
+   }
+/******************************************************
+ * HELPER FUNCTIONS
+ *
+ *******************************************************/
+
+
+ getPublishersData(authorData){
+   var publisherMarkers = [];
+   _.forEach(authorData, author => {
+    //check for each publisher. if long/lat data exists, create an object and add it to publisherMarkers
     if(author.publisher_latitude_1 && author.publisher_longitude_1){
-      var pubXY = projection([author.publisher_longitude_1, author.publisher_latitude_1])
+      const pub1 = {
+        id: author.id,
+        publisher_name: author.publisher_name_1,
+        publisher_longitude:  author.publisher_longitude_1,
+        publisher_latitude: author.publisher_latitude_1
+      }
+      publisherMarkers.push(pub1);
+    }
+    if(author.publisher_latitude_2 && author.publisher_longitude_2){
+      const pub2 = {
+        id: author.id,
+        publisher_name: author.publisher_name_2,
+        publisher_longitude:  author.publisher_longitude_2,
+        publisher_latitude: author.publisher_latitude_2
+      }
+      publisherMarkers.push(pub2);
 
     }
-    //check if there is a marker for publisher 3, if so, create a line. if not, move to the next if statement
-    //cehck if there is a marker for lastworkplace, if so, create a line
+    if(author.publisher_latitude_3 && author.publisher_longitude_3){
+      const pub3 = {
+        id: author.id,
+        publisher_name: author.publisher_name_3,
+        publisher_longitude:  author.publisher_longitude_3,
+        publisher_latitude: author.publisher_latitude_3
+      }
+      publisherMarkers.push(pub3);
+    }
 
+   })
+
+  return publisherMarkers;
+ }
+  getGeoJSONCoord(author){
+    var authorCoords = []
+    const authorBirthCoords = [parseFloat(author.birthplace_longitude), parseFloat(author.birthplace_latitude)];
+
+    //creates the coordinates to be used
+    if(author.publisher_latitude_1 && author.publisher_longitude_1) {
+      const publisher_coords1 = [authorBirthCoords, [parseFloat(author.publisher_longitude_1), parseFloat(author.publisher_latitude_1)]]
+      authorCoords.push(publisher_coords1);
+    }
+
+    if(author.publisher_latitude_2 && author.publisher_longitude_2) {
+      const publisher_coords2 = [authorBirthCoords, [parseFloat(author.publisher_longitude_2), parseFloat(author.publisher_latitude_2)]]
+      authorCoords.push(publisher_coords2);
+    }
+
+    if(author.publisher_latitude_3 && author.publisher_longitude_3) {
+      const publisher_coords3 = [authorBirthCoords, [parseFloat(author.publisher_longitude_3), parseFloat(author.publisher_latitude_3)]]
+      authorCoords.push(publisher_coords3);
+    }
+
+    if(author.last_workplace_latitude && author.last_workplace_longitude) {
+      const last_workplace_coords = [authorBirthCoords, [parseFloat(author.last_workplace_longitude), parseFloat(author.last_workplace_latitude)]]
+      authorCoords.push(last_workplace_coords);
+    }
+
+    return authorCoords;
+  }
+/**
+ * GetGeoJsonObj : Creates a GeoJson object from the current authors data, and returns the finished object
+ */
+  getGeoJsonObj(author){
+    const authorGeoCoord = this.getGeoJSONCoord(author);
+    var newFeature;
+    var authorGeo = {};
+    //Set up GeoJson skeleton
+    authorGeo['type'] = 'MultiLineString';
+    authorGeo['coordinates'] = authorGeoCoord;
+
+    return authorGeo;
   }
 
-  drawLine(x1, y1, x2, y2, type){
 
-  }
+
+
 
 
 
